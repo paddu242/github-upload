934ce1be79f2b062ef8c0f36cbe8a27d9d3adb40
Padmini Pinaka <paddu0242@gmail.com>
2020-05-20 16:47:58 +0400

    Initial Commit

---
+++ CardData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardData
+//
+//  Created by Rings Technology on 5/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
