8834a3005f89c988522d93b24b6ca39715e9dfbc
Padmini Pinaka <paddu0242@gmail.com>
2020-05-24 17:43:57 +0400

    initializing repository

---
+++ CardData/CardDataCell.swift
@@ -0,0 +1,23 @@
+//
+//  CardDataCell.swift
+//  CardData
+//
+//  Created by Rings Technology on 5/20/20.
+//
+
+import UIKit
+
+class CardDataCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
